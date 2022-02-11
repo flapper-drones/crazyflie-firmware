9ceb583b943c2feb7296024cd6da9b182fd9af09
matejkarasek <matejkarasek@gmail.com>
2022-02-11 17:26:55 +0100

    Rearrange config

--- tools/make/config.mk
+++ tools/make/config.mk
@@ -2,19 +2,11 @@
 #### Config for Flapper Drone Nimble+ ####
 ##########################################
 
-## Do not change the settings below, unless if you know what you are doing ##
-#############################################################################
 
-## Uncomment when using CF2 as flight controller
-# CFLAGS += -DNIMBLE_USE_CF2
+## Uncomment the following block of lines depending on the PCB version used ##
+##############################################################################
 
-CFLAGS += -DMOTOR_SETUP_NIMBLE
-# CFLAGS += -DENABLE_PWM_EXTENDED
-## Keep the yaw setpoint within +/- YAW_MAX_DELTA from the current heading
-CFLAGS += -DYAW_MAX_DELTA=30.0f
-CFLAGS += -DPID_FILTER_ALL
-CFLAGS += -DCPPM_USE_PA3 # CPPM pin, other alternatives:  PA7 (default), PA2(TX2), PA3(RX2), PB4(IO_3), PB5(IO_2) or PB8(IO_1)
-
+## Model 2021:
 # POWER_DISTRIBUTION = nimble_FD_PCB
 # CFLAGS += -DMOTOR_SERVO_PITCH=1
 # CFLAGS += -DMOTOR_SERVO_YAW=2
@@ -22,12 +14,14 @@ CFLAGS += -DCPPM_USE_PA3 # CPPM pin, other alternatives:  PA7 (default), PA2(TX2
 # CFLAGS += -DIMU_THETA=90.0f
 # CFLAGS += -DIMU_PSI=180.0f
 
-POWER_DISTRIBUTION = nimble_FD_PCB_revD
-CFLAGS += -DMOTOR_SERVO_PITCH=0
-CFLAGS += -DMOTOR_SERVO_YAW=2
-CFLAGS += -DIMU_PHI=0.0f # IMU orientation
-CFLAGS += -DIMU_THETA=-90.0f
-CFLAGS += -DIMU_PSI=180.0f
+# ## Model 2022 (revision D):
+# POWER_DISTRIBUTION = nimble_FD_PCB_revD
+# CFLAGS += -DMOTOR_SERVO_PITCH=0
+# CFLAGS += -DMOTOR_SERVO_YAW=2
+# CFLAGS += -DIMU_PHI=0.0f # IMU orientation
+# CFLAGS += -DIMU_THETA=-90.0f
+# CFLAGS += -DIMU_PSI=180.0f
+# CFLAGS += -DPM_CURRENT_FILTER_ALPHA=0.975f
 
 ## Deck selection ##
 ####################
@@ -39,8 +33,7 @@ CFLAGS += -DIMU_PSI=180.0f
 # CFLAGS += -DDECK_FORCE=bcCurrentDeck # analog current sensor on PA2/TX2 (integrated only in PCB_revD)
 
 ## To use multiple features at a time, separate their names with colons:
-# CFLAGS += -DDECK_FORCE=bcUSD:bcCPPM:bcLedRing:bcCurrentDeck
-CFLAGS += -DDECK_FORCE=bcUSD:bcLedRing:bcCurrentDeck
+CFLAGS += -DDECK_FORCE=bcUSD:bcLedRing
 
 ## External receiver settings ##
 ################################
@@ -61,3 +54,18 @@ CFLAGS += -DDECK_FORCE=bcUSD:bcLedRing:bcCurrentDeck
 ## Limit maximal thrust (100% = 65535) ##
 #########################################
 # CFLAGS += -DNIMBLE_MAX_THRUST 53000.0f
+
+## Do not change the settings below, unless if you know what you are doing ##
+#############################################################################
+
+## Uncomment when using CF2 as flight controller
+# CFLAGS += -DNIMBLE_USE_CF2
+
+CFLAGS += -DMOTOR_SETUP_NIMBLE
+# CFLAGS += -DENABLE_PWM_EXTENDED
+
+CFLAGS += -DYAW_MAX_DELTA=30.0f # Keep the yaw setpoint within +/- YAW_MAX_DELTA from the current heading
+CFLAGS += -DPID_FILTER_ALL
+CFLAGS += -DCPPM_USE_PA3 # CPPM pin, other alternatives:  PA7 (default), PA2(TX2), PA3(RX2), PB4(IO_3), PB5(IO_2) or PB8(IO_1)
+CFLAGS += -DCRITICAL_LOW_VOLTAGE=6.4f
+CFLAGS += -DLOW_VOLTAGE=6.8f
